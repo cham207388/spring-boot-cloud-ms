44ed21eff3c0b5a38b718ae77adfdd3d3225d971
Alhagie Bai Cham <cham207388@gmail.com>
2023-02-14 15:48:45 -0600

    using feign is working

--- employee-service/src/main/java/com/abc/ms/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/abc/ms/EmployeeServiceApplication.java
@@ -2,10 +2,10 @@ package com.abc.ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-//@EnableFeignClients
+@EnableFeignClients
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
