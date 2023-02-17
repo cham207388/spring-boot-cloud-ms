ab65fda9a6589fa66de874c1e918619dc32e4583
Alhagie Bai Cham <cham207388@gmail.com>
2023-02-17 15:51:45 -0600

    org service

--- api-gateway/src/main/java/com/abc/ms/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/abc/ms/config/SecurityConfig.java
@@ -1,28 +1,28 @@
-package com.abc.ms.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-
-/**
- * @author Alhagie Bai Cham
- * @date 2/16/23
- */
-public class SecurityConfig {
-    private final static String DEPARTMENT_RESOURCE = "department-service/api/departments/**";
-    private final static String EMPLOYEE_RESOURCE = "employee-service/api/employees/**";
-    private final static String ORGANIZATION_RESOURCE = "organization-service/api/organizations/**";
-    private final static String EUREKA_RESOURCE = "http://localhost:8671/**";
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers(DEPARTMENT_RESOURCE, EMPLOYEE_RESOURCE, EUREKA_RESOURCE, ORGANIZATION_RESOURCE)
-                                .permitAll()
-                                .anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        return http.build();
-    }
-}
+//package com.abc.ms.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+///**
+// * @author Alhagie Bai Cham
+// * @date 2/16/23
+// */
+//public class SecurityConfig {
+//    private final static String DEPARTMENT_RESOURCE = "department-service/api/departments/**";
+//    private final static String EMPLOYEE_RESOURCE = "employee-service/api/employees/**";
+//    private final static String ORGANIZATION_RESOURCE = "organization-service/api/organizations/**";
+//    private final static String EUREKA_RESOURCE = "http://localhost:8671/**";
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.csrf().disable().authorizeHttpRequests(
+//                        authorize -> authorize.requestMatchers(DEPARTMENT_RESOURCE, EMPLOYEE_RESOURCE, EUREKA_RESOURCE, ORGANIZATION_RESOURCE)
+//                                .permitAll()
+//                                .anyRequest().authenticated())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.csrf().disable();
+//        return http.build();
+//    }
+//}
