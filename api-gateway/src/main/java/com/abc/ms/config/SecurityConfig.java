6a102f0429756733512f37045a7065fee722a1e3
Alhagie Bai Cham <cham207388@gmail.com>
2023-02-17 15:11:44 -0600

    refactor

--- api-gateway/src/main/java/com/abc/ms/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/abc/ms/config/SecurityConfig.java
@@ -12,12 +12,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     private final static String DEPARTMENT_RESOURCE = "department-service/api/departments/**";
     private final static String EMPLOYEE_RESOURCE = "employee-service/api/employees/**";
+    private final static String ORGANIZATION_RESOURCE = "organization-service/api/organizations/**";
     private final static String EUREKA_RESOURCE = "http://localhost:8671/**";
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeHttpRequests(
-                        authorize -> authorize.requestMatchers(DEPARTMENT_RESOURCE, EMPLOYEE_RESOURCE, EUREKA_RESOURCE)
+                        authorize -> authorize.requestMatchers(DEPARTMENT_RESOURCE, EMPLOYEE_RESOURCE, EUREKA_RESOURCE, ORGANIZATION_RESOURCE)
                                 .permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
